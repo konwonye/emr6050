3619dd5d5930f5c1e7011faefb329dd9fde6ede3
konwonye <52248911+konwonye@users.noreply.github.com>
2019-12-05 18:08:42 -0600

    Add files via upload

---
+++ src/main/java/hello/MvcConfig.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/index").setViewName("index");
+    registry.addViewController("/").setViewName("index");
+    registry.addViewController("/uploadForm").setViewName("uploadForm");
+    registry.addViewController("/login").setViewName("login");
+  }
+
+}
---
+++ src/test/java/hello/Emr6050ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Emr6050ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
